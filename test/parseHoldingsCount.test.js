ca117db289c338167b29a18c2f45c9d08ea97a8e
Coombs,Karen <coombsk@oclc.org>
2020-07-13 16:17:40 -0500

    Added test to parse holding counts

--- test/parseHoldingsCount.test.js
+++ test/parseHoldingsCount.test.js
@@ -5,7 +5,7 @@ const gas = require('gas-local');
 const lib = gas.require('./src');
 
 holding_count_response = fs.readFileSync(require('path').resolve(__dirname, './mocks/holding_count_response.json')).toString();
-holding_count_multiple_editions_response = fs.readFileSync(require('path').resolve(__dirname, './mocks/holding_count_multiple_editions_response.json')).toString();
+holding_count_multiple_editions_response = fs.readFileSync(require('path').resolve(__dirname, './mocks/holding_count_multiple_editions.json')).toString();
 
 describe('parse holding count', () => {
 	it('parses result with 1 edition', () => {
